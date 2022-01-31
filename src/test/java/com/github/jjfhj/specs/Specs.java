c9123cf32c685c60ccb8854828dbf0f23a3fcee4
Karina <aiwpront@gmail.com>
2022-02-01 00:54:37 +0400

    added specification

---
+++ src/test/java/com/github/jjfhj/specs/Specs.java
@@ -0,0 +1,25 @@
+package com.github.jjfhj.specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.filter.log.LogDetail.HEADERS;
+
+public class Specs {
+    public static RequestSpecification request = with()
+            .baseUri("https://reqres.in")
+            .basePath("/api")
+            .log().all()
+            .contentType(ContentType.JSON);
+
+    public static ResponseSpecification responseSpec = new ResponseSpecBuilder()
+            .log(HEADERS)
+            .log(BODY)
+            .expectStatusCode(200)
+//            .expectBody(containsString("success"))
+            .build();
+}
